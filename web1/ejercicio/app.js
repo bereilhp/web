f3ced23abb151090e2b20c2ad20e4037009d7d37
PedroB <bereilhp@gmail.com>
2022-12-06 16:14:36 +0100

    status-monitor

--- web1/ejercicio/app.js
+++ web1/ejercicio/app.js
@@ -20,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('express-status-monitor')());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
