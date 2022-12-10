68d57f221840ed3b18c924a9195e0b1c35ade6c9
PedroB <bereilhp@gmail.com>
2022-12-10 16:08:43 +0100

    fix register

--- web1/tienda/routes/registro.js
+++ web1/tienda/routes/registro.js
@@ -9,33 +9,24 @@ router.get('/', function(req, res, next) {
 
 router.post("/",function(req, res, next) {
 
+  console.log(req.session.user);
   const user = req.body.user;
   const pass = req.body.pass; //agarrar name de input
   const pass1 = req.body.pass1;
 
-  if(pass === pass1){
-    if(pass.length == 8 ){
-
-    
-    users.register(user,pass,function(){
-      console.log("Se registro con exito");
-    })
-
-    req.session.message = "Welcome";
-    res.redirect("/restricted");
-
-  }else {
-    req.session.error = "sdfsdnfjsdfijj"
+  if(pass != pass1){
+    req.session.error = "Passwords don't match";
     res.redirect("/registro");
+  }else if (pass.length < 8 && pass1.length < 8){
+    req.session.error = "Passwords need to be 8 characters long";
+    res.redirect("/registro");
+  } else if (pass === pass1){
+    users.register(user,pass,function(){
+      console.log("Se ha registrado con exito");
+    });
+    req.session.message = "You have just registered please enter new account to log in!";
+    res.redirect("/login");
   }
-} else {
-  req.session.error = "La contraseña no coincide"
-  res.redirect("/registro");
-}
-
 });
-
-
-  
 
 module.exports = router;
