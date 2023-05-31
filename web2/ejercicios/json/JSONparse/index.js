86b8e1b2c8aa064f4919d34420085821c76ce230
PedroB <bereilhp@gmail.com>
2023-05-31 12:32:03 +0200

    json parse

--- web2/ejercicios/json/JSONparse/index.js
+++ web2/ejercicios/json/JSONparse/index.js
@@ -2,5 +2,6 @@ const jsonString = `{"name": "John Doe", "age": 30, "city": "New York"}`;
 
 const jsonObj = JSON.parse(jsonString);
 
+console.log(jsonString.name)
 console.log(jsonObj)
 console.log(jsonObj.name)
